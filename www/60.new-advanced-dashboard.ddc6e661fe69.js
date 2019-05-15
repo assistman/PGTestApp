152ad17aef4939bb5779f45ab45a71bd89eca414
Stanislav Razbiegin <Stanislav.Razbiegin@kstt.ua>
2019-05-15 18:56:28 +0300

    Code updated from cosmos-light master

---
+++ www/60.new-advanced-dashboard.ddc6e661fe69.js
@@ -0,0 +1,11 @@
+webpackJsonp([60],{
+
+/***/ 2035:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("Object.defineProperty(__webpack_exports__, \"__esModule\", { value: true });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"NewAdvancedDashboard\", function() { return NewAdvancedDashboard; });\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(4);\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__DashboardLazyModules__ = __webpack_require__(852);\n\n\n\n\nvar NewAdvancedDashboard = function NewAdvancedDashboard() {\n    return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1__DashboardLazyModules__[\"LazyNewAdvancedDashboardDesktop\"], null);\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (NewAdvancedDashboard);\n\n//////////////////\n// WEBPACK FOOTER\n// ./src/js/modules/dashboard/components/NewAdvancedDashboard.jsx\n// module id = 2035\n// module chunks = 60\n\n//# sourceURL=webpack:///./src/js/modules/dashboard/components/NewAdvancedDashboard.jsx?");
+
+/***/ })
+
+});
